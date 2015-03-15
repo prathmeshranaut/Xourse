414fa4d40b845062ca02161fa3f1f995c8ea4c65
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-03-15 16:16:18 +0530

    Fixed styling issu when pushed to different server

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,8 +1,8 @@
 <html>
 <head>
     <title>@yield('title', 'Xourse')</title>
-    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/style.css">
+    {{ HTML::style('bower_components/bootstrap/dist/css/bootstrap.min.css') }}
+    {{ HTML::style('assets/style.css') }}
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     </head>
 <body>
